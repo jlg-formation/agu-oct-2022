bf58254e03722659d3fdf302439b16a7e7d5d294
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-10-21 09:40:02 +0200

    dev: added directive autofocus

--- front/src/app/stock/stock.module.ts
+++ front/src/app/stock/stock.module.ts
@@ -5,6 +5,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CreateComponent } from './create/create.component';
 import { StockRoutingModule } from './stock-routing.module';
 import { StockComponent } from './stock.component';
+import { WidgetModule } from '../widget/widget.module';
 
 @NgModule({
   declarations: [StockComponent, CreateComponent],
@@ -13,6 +14,7 @@ import { StockComponent } from './stock.component';
     StockRoutingModule,
     ReactiveFormsModule,
     FontAwesomeModule,
+    WidgetModule,
   ],
 })
 export class StockModule {}
